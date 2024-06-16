5e1b4e2110276f04d6f910d0f3d569085b4d5e5f
Daniel Seifert <git@daniel-seifert.com>
2024-06-16 13:53:57 +0200

    align copyright header

--- src/Production/IsMockable.php
+++ src/Production/IsMockable.php
@@ -1,8 +1,10 @@
 <?php
 
 /**
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * Copyright (c) D3 Data Development (Inh. Thomas Dartsch)
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
  *
  * https://www.d3data.de
  *
